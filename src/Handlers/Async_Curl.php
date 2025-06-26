fb74ee6eb1b8b11391b63420e910edab45f2010c
Joseph Chunta <jjbchunta@gmail.com>
2025-06-26 00:08:49 -0400

    Nvm, overcomplicated things.

--- src/Handlers/Async_Curl.php
+++ src/Handlers/Async_Curl.php
@@ -2,40 +2,16 @@
 
 namespace Jjbchunta\Async\Handlers;
 
-use Jjbchunta\Async\Handlers\AsyncInterface;
 use Jjbchunta\Async\Handlers\Async_Script;
 
-class Async_Curl implements AsyncInterface {
-    protected $handler;
-
+class Async_Curl extends Async_Script {
     public static function is_process_of_type( $process ) {
         return true;
     }
 
     public function __construct( $process ) {
-        $dir = rtrim( __DIR__, '/\\' ) . '/';
-        $executable = "{$dir}../bin/Curl.php";
+        $executable = __DIR__ . "/../bin/Curl.php";
         $command = "$executable $process";
-        $this->handler = new Async_Script( $command );
-    }
-
-    public function is_running() {
-        return $this->handler->is_running();
-    }
-
-    public function wait() {
-        return $this->handler->wait();
-    }
-
-    public function get_exit_code() {
-        return $this->handler->get_exit_code();
-    }
-
-    public function stop() {
-        return $this->handler->stop();
-    }
-
-    public function result() {
-        return $this->handler->result();
+        parent::__construct( $command );
     }
 }
